8a86e62f9420f1c1383be6685aa35825012d17d5
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-14 19:01:24 +0200

    added restrictions for username size and email form through annotations

--- backend/src/main/java/de/mediasystem/backend/api/dto/RegisterRequest.java
+++ backend/src/main/java/de/mediasystem/backend/api/dto/RegisterRequest.java
@@ -1,5 +1,13 @@
 package de.mediasystem.backend.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 // Diese Klasse gibt die benötigte Datenstruktur vor.
-public record RegisterRequest(String username, String email, String password, String codeword) { }
+public record RegisterRequest(
+        @NotBlank @Size(min = 4, max = 15) String username,
+        @NotBlank @Email String email,
+        @NotBlank String password,
+        String codeword) { }
 
