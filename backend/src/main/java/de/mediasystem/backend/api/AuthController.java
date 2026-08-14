63ccd32b82971efec0f4907adb41d9f3420c41f7
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-15 00:43:46 +0200

    Created the first Controller Method for handling the Post Request from the Frontend and returning a response-status + body

--- backend/src/main/java/de/mediasystem/backend/api/AuthController.java
+++ backend/src/main/java/de/mediasystem/backend/api/AuthController.java
@@ -1,4 +1,27 @@
 package de.mediasystem.backend.api;
 
+import de.mediasystem.backend.api.dto.RegisterRequest;
+import de.mediasystem.backend.model.roles.User;
+import jakarta.validation.Valid;
+import lombok.NonNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import de.mediasystem.backend.service.AuthService;
+
+@RestController
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<@NonNull User> register(@RequestBody @Valid RegisterRequest request) {
+        return ResponseEntity.status(201).body(authService.register(request));
+    }
+
 }
