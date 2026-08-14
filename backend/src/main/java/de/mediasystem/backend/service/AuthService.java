effe18ed5c1e031dc233f753d75451478ff5bacf
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-14 18:35:42 +0200

    implemented the first version of the register method

--- backend/src/main/java/de/mediasystem/backend/service/AuthService.java
+++ backend/src/main/java/de/mediasystem/backend/service/AuthService.java
@@ -1,4 +1,47 @@
 package de.mediasystem.backend.service;
 
+import de.mediasystem.backend.api.dto.RegisterRequest;
+import de.mediasystem.backend.db.AppSettingRepository;
+import de.mediasystem.backend.db.UserRepository;
+import de.mediasystem.backend.model.AppSetting;
+import de.mediasystem.backend.model.roles.User;
+import de.mediasystem.backend.service.exception.InvalidCodewordException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final AppSettingRepository appSettingRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository,
+                       AppSettingRepository appSettingRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.appSettingRepository = appSettingRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(RegisterRequest request) {
+        if (userRepository.count() >= 10) {
+            AppSetting appSetting = appSettingRepository.findById("signup_codeword")
+                    // Intellij meinte ich soll das mit :: machen anstatt lambda.
+                    .orElseThrow(InvalidCodewordException::new);
+            if (!Objects.equals(request.codeword(),
+                    appSetting.getValue())) {
+                throw new InvalidCodewordException();
+            }
+        }
+        User user = new User();
+        user.setUsername(request.username());
+        user.setEmail(request.email());
+        user.setPasswordHash(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+
+        return user;
+    }
 }
