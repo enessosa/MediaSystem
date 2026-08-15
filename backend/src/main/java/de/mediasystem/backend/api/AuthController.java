7148b08f4f71c8f44d03bdcfb8187182ca0e2db9
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-15 09:43:42 +0200

    implemented the Api-layer for Logins to communicate with the frontend

--- backend/src/main/java/de/mediasystem/backend/api/AuthController.java
+++ backend/src/main/java/de/mediasystem/backend/api/AuthController.java
@@ -1,10 +1,17 @@
 package de.mediasystem.backend.api;
 
+import de.mediasystem.backend.api.dto.LoginRequest;
 import de.mediasystem.backend.api.dto.RegisterRequest;
 import de.mediasystem.backend.model.roles.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +21,27 @@ import de.mediasystem.backend.service.AuthService;
 public class AuthController {
 
     private final AuthService authService;
+    private final SecurityContextRepository repository;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, SecurityContextRepository repository) {
         this.authService = authService;
+        this.repository = repository;
     }
 
     @PostMapping("/auth/register")
     public ResponseEntity<@NonNull User> register(@RequestBody @Valid RegisterRequest request) {
         return ResponseEntity.status(201).body(authService.register(request));
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<@NonNull Authentication> login(@RequestBody @Valid LoginRequest request,
+                                                         HttpServletRequest httpServletRequest,
+                                                         HttpServletResponse httpServletResponse) {
+        Authentication auth = authService.login(request);
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(auth);
+        repository.saveContext(context, httpServletRequest, httpServletResponse);
+        return ResponseEntity.ok(auth);
     }
 
 }
--- backend/src/main/java/de/mediasystem/backend/config/SecurityConfig.java
+++ backend/src/main/java/de/mediasystem/backend/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -48,6 +50,11 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 
 }
