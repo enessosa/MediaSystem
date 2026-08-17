3c494671ac63286bc48b4a92cf3a06e1bb99f8ee
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-17 14:07:36 +0200

    created the controller method for the REST-API connection between backend and frontend. started with the functionality of searching media.

--- backend/src/main/java/de/mediasystem/backend/api/MediaController.java
+++ backend/src/main/java/de/mediasystem/backend/api/MediaController.java
@@ -1,4 +1,25 @@
 package de.mediasystem.backend.api;
 
+import de.mediasystem.backend.api.dto.MediaSearchResult;
+import de.mediasystem.backend.service.MediaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MediaController {
+
+    private final MediaService mediaService;
+
+    public MediaController(MediaService mediaService) {
+        this.mediaService = mediaService;
+    }
+
+    @GetMapping("/media/search")
+    public ResponseEntity<List<MediaSearchResult>> searchResult(@RequestParam String q) {
+        return ResponseEntity.ok(mediaService.searchMedia(q));
+    }
 }
