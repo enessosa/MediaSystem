4314956f89c65f8047a146fa136290c5e8edaa4e
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-20 00:00:43 +0200

    added an Exception message

--- backend/src/main/java/de/mediasystem/backend/service/exception/AlreadyInListException.java
+++ backend/src/main/java/de/mediasystem/backend/service/exception/AlreadyInListException.java
@@ -1,3 +1,7 @@
 package de.mediasystem.backend.service.exception;
 
-public class AlreadyInListException extends RuntimeException { }
+public class AlreadyInListException extends RuntimeException {
+    public AlreadyInListException(String message) {
+        super(message);
+    }
+}
