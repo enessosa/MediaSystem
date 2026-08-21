e6ec14378440fc4b002e7d7dd7e09f67cf87a711
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-21 21:18:23 +0200

    implemented a postmapping method in the Controller Class

--- backend/src/main/java/de/mediasystem/backend/api/MediaController.java
+++ backend/src/main/java/de/mediasystem/backend/api/MediaController.java
@@ -1,12 +1,13 @@
 package de.mediasystem.backend.api;
 
 import de.mediasystem.backend.api.dto.MediaSearchResult;
+import de.mediasystem.backend.api.dto.UserEntryResponse;
 import de.mediasystem.backend.service.MediaService;
+import de.mediasystem.backend.service.UserService;
 import lombok.NonNull;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +15,25 @@ import java.util.List;
 public class MediaController {
 
     private final MediaService mediaService;
+    private final UserService userService;
 
-    public MediaController(MediaService mediaService) {
+    public MediaController(MediaService mediaService, UserService userService) {
         this.mediaService = mediaService;
+        this.userService = userService;
     }
 
     @GetMapping("/media/search")
     public ResponseEntity<@NonNull List<MediaSearchResult>> searchResult(@RequestParam String q) {
         return ResponseEntity.ok(mediaService.searchMedia(q));
+    }
+
+
+    @PostMapping("/media/entries")
+    public ResponseEntity<@NonNull UserEntryResponse> addToUserList(@RequestBody MediaSearchResult result, Authentication authentication) {
+        String username = authentication.getName();
+        Long userId = userService.getUserIdByUsername(username);
+        UserEntryResponse userEntryResponse = mediaService.addToUserList(result, userId);
+
+        return ResponseEntity.status(201).body(userEntryResponse);
     }
 }
