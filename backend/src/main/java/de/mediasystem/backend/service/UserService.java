f47b8f48ece62a9a20e9e89b8ba88bce6283442b
enessosa <102462614+enessosa@users.noreply.github.com>
2026-08-21 21:16:55 +0200

    implemented a method to get the UserId with the Username to map an Item to the Entry.

--- backend/src/main/java/de/mediasystem/backend/service/UserService.java
+++ backend/src/main/java/de/mediasystem/backend/service/UserService.java
@@ -1,4 +1,23 @@
 package de.mediasystem.backend.service;
 
+
+import de.mediasystem.backend.db.UserRepository;
+import de.mediasystem.backend.model.roles.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Long getUserIdByUsername(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow();
+        return user.getId();
+    }
 }
